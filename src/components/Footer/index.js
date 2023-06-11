58b2897cabf1bd1d8252482f4a80ef89aaa66d51
RaykoBzh29 <36403005+RaykoBzh29@users.noreply.github.com>
2023-06-11 21:54:42 +0200

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,22 @@
 import React from "react";
 import "./style.css";
-import './polygon/style.css';
-import './generalCondition/style.css';
+import "./polygon/style.css";
+import "./generalCondition/style.css";
 import "./linkList/style.css";
 import LinkListPath from "./linkList/index.js";
-import GeneralCondition from './generalCondition/index';
-import Polygon from './polygon/index';
+import GeneralCondition from "./generalCondition/index";
+import Polygon from "./polygon/index";
 
-
-
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="footerContainer">
-          <LinkListPath />
-          <GeneralCondition />
-          <Polygon />
-        </div>
-      </footer>
-    );
-  }
+function Footer() {
+	return (
+		<footer className="footer">
+			<div className="footerContainer">
+				<LinkListPath />
+				<GeneralCondition />
+				<Polygon />
+			</div>
+		</footer>
+	);
 }
-
 
 export default Footer;
