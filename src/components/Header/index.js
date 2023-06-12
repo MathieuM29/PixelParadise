a14b71b540cd01cead2a478fe73fb731a693d934
MathieuM29 <marcmathieu29@gmail.com>
2023-06-12 18:39:13 +0200

    work on menu toggle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
 import './style.css';
+import Menu from './Menu';
+import { useState } from "react";
 
-function Header() {
+
+function Header({ isVisible, setIsVisible }) {
+
+  // const handleIsVisible = () => {
+
+  //   setIsVisible(!isVisible);
+  // };
+
   return (
     <header className="header">
 
       <div className="header__menu">
-        <i className="header__menu--icon fa-solid fa-bars"></i>
+        <i className="header__menu--icon fa-solid fa-bars"onClick={() => setIsVisible(!isVisible)}></i>
+        
+        {isVisible && <Menu />}
       </div>
 
       <div className="header__title">
@@ -14,7 +25,7 @@ function Header() {
       
       <div className="header__icons">
         <i className="fa-solid fa-circle-user"></i>
-        <i class="fa-solid fa-magnifying-glass"></i>
+        <i className="fa-solid fa-magnifying-glass"></i>
       </div>
 
     </header>
