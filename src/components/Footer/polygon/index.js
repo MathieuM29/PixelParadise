30bf42a6f7ff47fc53560c4c5c36da5e539072b6
MathieuM29 <marcmathieu29@gmail.com>
2023-06-15 23:25:38 +0200

    creepy class component into pretty function component

--- src/components/Footer/polygon/index.js
+++ src/components/Footer/polygon/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./_style.scss";
 
 
-class Polygon extends React.Component {
-  render() {
+const Polygon = () => {
     return (
       <div className="polygonContainer">
 				<div className="polygon"></div>
 			</div>
     );
-  }
+  
 }
 
 export default Polygon;
